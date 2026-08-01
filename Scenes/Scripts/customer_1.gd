66458e5abaa17f0c8e53ec31b195d04f8afcc929
Nadoooor <nadersayed742@gmail.com>
2026-08-02 05:42:55 +0800

    UHHHHh, finally wrote the random ordering system

--- Scenes/Scripts/customer_1.gd
+++ Scenes/Scripts/customer_1.gd
@@ -1,9 +1,9 @@
 extends Node2D
 signal ordered(recipe_name)
 @export var target_position : Vector2
-@export var move_duration: float = 2.5
-@export var possible_orders : Array[String] = ['ramen','eggs','chiken','beef']
-
+@export var move_duration: float = 1.5
+var ing = ["Beef", "Egg", "Veggie"]
+var order = []
 #@onready var sprite = $Sprite2D
 func setup_customer(spawn_pos : Vector2, target_pos : Vector2, visual_texture: Texture2D) -> void:
 	global_position = spawn_pos
@@ -17,14 +17,25 @@ func setup_customer(spawn_pos : Vector2, target_pos : Vector2, visual_texture: T
 	# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
+	
 
-	
-	
 # for the orders
 func _on_reached_destination() -> void :
-	var randomw_order = possible_orders.pick_random()
-	emit_signal("ordered",randomw_order)
+	var no_of_ings = randi_range(1,3)
+	print(no_of_ings)
+	var i = no_of_ings
+	while i>0:
+		var ind = randi_range(1, 3) - 1
+		if order.count(ing[ind]) >= 1 :
+			continue
+		else:
+			order.append(ing[ind])
+			print(order)
+		i-=1
+		 
 	
-	# Called every frame. 'delta' is the elapsed time since the previous frame.
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
--- Scenes/Scripts/customer_Spawner.gd
+++ Scenes/Scripts/customer_Spawner.gd
@@ -3,25 +3,43 @@ extends Node2D
 @export var customer_scene : PackedScene
 @export var customer_sprites : Array[Texture2D]
 var spawn_postion  = Vector2(-574,180)
-var counter_position = Vector2(466,180)
+var counter_position = Vector2(-100,180)
+var customers_lapse = 0
+var rand = 0
 
 func _on_timer_timeout() -> void:
-	if customer_sprites.is_empty():
+	if customer_sprites.is_empty() or customers_lapse >= 3:
 		return
 		
 	var new_customer = customer_scene.instantiate()
 	get_tree().current_scene.add_child(new_customer) 
 	
 	var random_look = customer_sprites.pick_random()
-	new_customer.setup_customer(spawn_postion, counter_position, random_look) 
-	new_customer.ordered.connect(_on_customer_ordered) 
-func _on_customer_ordered(order_name : String ) ->void :
+	new_customer.setup_customer(spawn_postion, counter_position, random_look)
 	print("order done")
+	print(customers_lapse)
 	print(counter_position)
+	customers_lapse+=1
+	counter_position-= Vector2(170,0)
+	rand = randi_range(5, 20)
+	print(rand)
+	%guestcoming.wait_time = rand	
+	print(%guestcoming.wait_time)
+	%guestcoming.start()
+	print(customers_lapse)
 	
-# Called when the node enters the scene tree for the first time.
+	
+	
+#func _on_customer_ordered(order_name : String ) ->void :
+
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	rand = randi_range(5, 10)
+	print(rand)
+	%guestcoming.wait_time = rand	
+	print(%guestcoming.wait_time)
+	%guestcoming.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
